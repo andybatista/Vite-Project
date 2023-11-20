4425b3b26631bae6c0b266940d78d236e423c736
Andy Batista <andybatista@andys-air.mynetworksettings.com>
2023-11-20 01:45:54 -0500

    final"
    q
    git init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
-import './App.css'
+import { useState } from "react";
+import { puppyList } from "./src/data.js";
+import "./index.css";
+import "./App.css";
 
+function App() {
+  const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
+  const handleClick = (obj) => {
+    setFeatPupId(obj.id);
+  };
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
 
   return (
     <>
-      <div></div>
+      <div className="App">
+    {puppies.map((puppy) => {
+      return (
+            <p
+              onClick={() => {
+                handleClick(puppy);
+              }}
+              key={puppy.id}
+            >
+              {puppy.name}
+            </p>
+          );
+        })}
+
+    {featPupId && (
+     <div id="puppyCard">
+       <h2>{featuredPup.name}</h2>
+        <ul>
+        <li>Age: {featuredPup.age}</li>
+       <li>Email: {featuredPup.email}</li>
+        </ul>
+      </div>
+        )}
+      </div>
     </>
-  )
+  );
+}
 
-
-export default App
+export default App;
